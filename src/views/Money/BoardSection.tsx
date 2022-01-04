cff1be0104f479cf933e806cb20c1630e483d3a9
91jzhu <j1712032377@163.com>
2022-01-04 14:35:04 +0800

    Board 功能基本完成

--- src/views/Money/BoardSection.tsx
+++ src/views/Money/BoardSection.tsx
@@ -1,6 +1,57 @@
 import styled from "styled-components";
+import * as React from "react";
+import {useState} from "react";
 
-const BoardSection = styled.section`
+const BoardSection:React.FunctionComponent=()=>{
+    const [output,setOutput]=useState("0")
+    const numChange=(e:React.MouseEvent)=>{
+        // setOutput(output=>output!=='0'?output+num:num)
+        const content=(e.target as HTMLButtonElement).textContent;
+        if(!content)return
+        switch (content){
+            case '0':setOutput(output=>output!=='0'?output+'0':'0');break;
+            case '1':setOutput(output=>output!=='0'?output+1:'1');break;
+            case '2':setOutput(output=>output!=='0'?output+2:'2');break;
+            case '3':setOutput(output=>output!=='0'?output+3:'3');break;
+            case '4':setOutput(output=>output!=='0'?output+4:'4');break;
+            case '5':setOutput(output=>output!=='0'?output+5:'5');break;
+            case '6':setOutput(output=>output!=='0'?output+6:'6');break;
+            case '7':setOutput(output=>output!=='0'?output+7:'7');break;
+            case '8':setOutput(output=>output!=='0'?output+8:'8');break;
+            case '9':setOutput(output=>output!=='0'?output+9:'9');break;
+            case '删除':setOutput(output=>output.length>1? output.slice(0,output.length-1):'0');break;
+            case '清空':setOutput('0');break;
+            case 'OK':
+            case '.':setOutput(output=>!output.includes('.')?output+'.':output);break;
+        }
+    }
+    // @ts-ignore
+    return (
+        <Wrapper>
+            <div className="output">
+                {output}
+            </div>
+            <div className="pad clearfix" onClick={numChange}>
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+                <button>删除</button>
+                <button>4</button>
+                <button>5</button>
+                <button>6</button>
+                <button>清空</button>
+                <button>7</button>
+                <button>8</button>
+                <button>9</button>
+                <button className="ok">OK</button>
+                <button className="zero">0</button>
+                <button className="dot">.</button>
+            </div>
+        </Wrapper>
+    )
+}
+
+const Wrapper = styled.section`
   display: flex;
   flex-direction: column;
 
